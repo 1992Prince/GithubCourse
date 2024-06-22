f80bff66d26b73e0c53e2dc510cd3f3b63f93bf2
Prince Pandey <princepandey155@gmail.com>
2024-06-22 17:48:20 +0530

    updated P2_Delete

--- src/main/java/advance/P2_Delete.java
+++ src/main/java/advance/P2_Delete.java
@@ -5,5 +5,6 @@ public class P2_Delete {
     public static void main(String[] args) {
 
         System.out.println("I am the richest");
+        System.out.println("I am the beyond richest");
     }
 }
