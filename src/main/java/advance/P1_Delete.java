546c6d2bf080723034e581c8e2d28d783acc6360
Prince Pandey <princepandey155@gmail.com>
2024-06-22 17:47:17 +0530

    adding new files

---
+++ src/main/java/advance/P1_Delete.java
@@ -0,0 +1,9 @@
+package advance;
+
+public class P1_Delete {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am the best");
+    }
+}
---
+++ src/main/java/advance/P2_Delete.java
@@ -0,0 +1,9 @@
+package advance;
+
+public class P2_Delete {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am the richest");
+    }
+}
