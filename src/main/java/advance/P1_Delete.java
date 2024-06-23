a6d66bfd39fce144cccd5264ccb259ede9687575
Prince Pandey <princepandey155@gmail.com>
2024-06-23 09:29:21 +0530

    Adding new code to P1 and P2 files

--- src/main/java/advance/P1_Delete.java
+++ src/main/java/advance/P1_Delete.java
@@ -4,6 +4,7 @@ public class P1_Delete {
 
     public static void main(String[] args) {
 
+        int i = 100000;
         System.out.println("I am the best");
     }
 }
--- src/main/java/advance/P2_Delete.java
+++ src/main/java/advance/P2_Delete.java
@@ -4,7 +4,7 @@ public class P2_Delete {
 
     public static void main(String[] args) {
 
-        System.out.println("I am the richest");
+        System.out.println("I am the richest person in the world");
         System.out.println("I am the beyond richest");
     }
 }
